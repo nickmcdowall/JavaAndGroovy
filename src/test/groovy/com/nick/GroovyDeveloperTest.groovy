588bd4b4569ef64d89adbcda0be1148a529305d4
Linker Lin <linker.lin@me.com>
2013-07-26 00:39:05 +0800

    Upgrade plugins

--- src/test/groovy/com/nick/GroovyDeveloperTest.groovy
+++ src/test/groovy/com/nick/GroovyDeveloperTest.groovy
@@ -1,11 +1,11 @@
 package com.nick
-import static org.junit.Assert.*
-import static org.mockito.MockitoAnnotations.*
-import static org.mockito.Mockito.*
-
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+
+import static org.mockito.Mockito.times
+import static org.mockito.Mockito.verify
+import static org.mockito.MockitoAnnotations.initMocks
 
 
 class GroovyDeveloperTest {
